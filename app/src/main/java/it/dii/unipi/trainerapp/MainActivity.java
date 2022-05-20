db8dc3b7e8299bfd3f28decea1f56c488a354261
enricollen <nello.enrico@gmail.com>
2022-05-20 15:21:13 +0200

    Skeleton of Settings for the app. At first launch it asks the Trainer's name and stores it on file (Internal dedicated app storage). Then it should add it on append after the "Welcome " label in the main page, but it only works when app is closed and reopened, and not at the first launch (dont know why).

--- app/src/main/java/it/dii/unipi/trainerapp/MainActivity.java
+++ app/src/main/java/it/dii/unipi/trainerapp/MainActivity.java
@@ -1,19 +1,34 @@
 package it.dii.unipi.trainerapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 
 import java.util.ArrayList;
 
 import it.dii.unipi.trainerapp.GATTserver.GATTServerActivity;
 import it.dii.unipi.trainerapp.athlete.Athlete;
 import it.dii.unipi.trainerapp.ui.AthleteAdapter;
+import it.dii.unipi.trainerapp.ui.SettingsActivity;
+import it.dii.unipi.trainerapp.utilities.Utility;
 
 public class MainActivity extends GATTServerActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private boolean settingsInizialized = false;
+    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+    private String trainerName;
+    private String fileName = "settingsDump.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +37,39 @@ public class MainActivity extends GATTServerActivity {
         // Devices with a display should not go to sleep
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         initializeAthletesList();
+
+        //firstly checks whether the trainer name has already been set
+        trainerName = Utility.readFromFile(this, fileName);
+
+        if(!trainerName.equals("")){
+            settingsInizialized=true;
+        }
+
+        if(!settingsInizialized) {
+            someActivityResultLauncher = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    result -> {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            trainerName = data.getStringExtra("trainerName");
+                            settingsInizialized = true;
+                            Toast.makeText(getApplicationContext(),"Settings saved successfully!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+            //send an intent to Settings Activity
+            openSomeActivityForResult();
+        }
+
+        TextView trainerNameLabel = (TextView) findViewById(R.id.welcomeLabel);
+        trainerNameLabel.append(trainerName);
     }
 
-    //this method should be in GATTServerActivity, and called by the OnCreate method above,
-    //but doing so it ends up giving an error about threads execution :(
+    public void openSomeActivityForResult() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        someActivityResultLauncher.launch(intent);
+    }
+
+    //TODO: this method should be moved somewhere else, waiting for GATTServer class to become a Service
     public void initializeAthletesList() {
         Log.i(TAG, "initializeAthletesList()");
         // Construct the data source

a6867303e92c4ece1088b6f066ecc5a5150a2ef5
Ruggero1912 <63967908+Ruggero1912@users.noreply.github.com>
2022-05-25 12:54:05 +0200

    HRservice UUID fixes

--- app/src/main/java/it/dii/unipi/trainerapp/GATTserver/profiles/athleteProfile/services/heartRateService/HeartRateService.java
+++ app/src/main/java/it/dii/unipi/trainerapp/GATTserver/profiles/athleteProfile/services/heartRateService/HeartRateService.java
@@ -10,8 +10,8 @@ import it.dii.unipi.trainerapp.GATTserver.profiles.athleteProfile.services.athle
 public class HeartRateService {
     private static final String TAG = AthleteInformationService.class.getSimpleName();
 
-    public static UUID HEART_RATE_SERVICE = UUID.fromString("180D");
-    public static UUID HEART_RATE_CHARACTERISTIC = UUID.fromString("2a37");
+    public static UUID HEART_RATE_SERVICE = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb");
+    public static UUID HEART_RATE_CHARACTERISTIC = UUID.fromString("00002a37-0000-1000-8000-00805f9b34fb");
 
     public static BluetoothGattService createHeartRateService(){
         BluetoothGattService service = new BluetoothGattService(HEART_RATE_SERVICE, BluetoothGattService.SERVICE_TYPE_PRIMARY);
